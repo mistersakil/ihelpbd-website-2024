0b58dbbd771a42ff2ec0594f1d3f30d36b8ade6e
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-03-19 16:29:26 +0600

    created frontend.layout.topbar

--- resources/views/components/frontend/layout/master.blade.php
+++ resources/views/components/frontend/layout/master.blade.php
@@ -27,54 +27,7 @@
             </div>
         </div>
     </div> --}}
-
-
-    <header class="top-header top-header-bg">
-        <div class="container-fluid">
-            <div class="row align-items-center">
-                <div class="col-lg-8 col-sm-7">
-                    <div class="header-left">
-                        <ul>
-                            <li>
-                                <i class='bx bx-phone'></i>
-                                <a href="tel:+8256987456">+8256987456</a>
-                            </li>
-                            <li>
-                                <i class='bx bx-envelope'></i>
-                                <a href="mailto:webmaster@example.com">info@ihelpkl.com</a>.<br>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-5">
-                    <div class="header-right">
-                        <ul>
-                            <li>
-                                <a href="https://www.facebook.com/" target="_blank">
-                                    <i class="bx bxl-facebook"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.instagram.com/" target="_blank">
-                                    <i class="bx bxl-instagram"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://twitter.com/" target="_blank">
-                                    <i class="bx bxl-twitter"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.youtube.com/" target="_blank">
-                                    <i class="bx bxl-youtube"></i>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
+    <x-frontend.layout.topbar></x-frontend.layout.topbar>
 
     <x-frontend.layout.navbar></x-frontend.layout.navbar>
 
