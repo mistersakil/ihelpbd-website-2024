cc3fe3842c473df8ac2507171ac702e4fbe8a49c
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-03-23 16:52:12 +0600

    home choose section done on home page

---
+++ resources/views/livewire/frontend/partials/home-choose-section.blade.php
@@ -0,0 +1,60 @@
+<div class="choose-area pt-100 pb-70">
+    <div class="container-fluid">
+        <div class="section-title text-center">
+            <span class="sp-title">Why Choose Us</span>
+            <h2>Why You will Give Us Priority</h2>
+        </div>
+        <div class="row pt-45 align-items-center justify-content-center">
+            <div class="col-lg-5">
+                <div class="choose-img">
+                    <img src="{{ Vite::asset('resources/frontend/images/choose-img1.png') }}" alt="Choose Images">
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="choose-leftside">
+                    <div class="row">
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="choose-card">
+                                <h3>Top Customer Service</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                </p>
+                                <i class="flaticon-customer-service"></i>
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="choose-card">
+                                <h3>Highly Personalized</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                </p>
+                                <i class="flaticon-document"></i>
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="choose-card">
+                                <h3>Passionate Talent</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                </p>
+                                <i class="flaticon-user"></i>
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="choose-card">
+                                <h3>Quick Response Team</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                </p>
+                                <i class="flaticon-wall-clock"></i>
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="choose-bg">
+                        <img src="{{ Vite::asset('resources/frontend/images/choose-bg.png') }} " alt="Choose Images">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
