3824943d57aef9dba2377f64169ffd7f69aaaa66
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-03-26 12:46:46 +0600

    updated frontend static images

--- resources/views/livewire/frontend/home/home-page.blade.php
+++ resources/views/livewire/frontend/home/home-page.blade.php
@@ -1,9 +1,9 @@
 <main>
-    {{-- <livewire:frontend.partials.home-banner />
+    <livewire:frontend.partials.home-banner />
     <livewire:frontend.partials.home-about-section />
     <livewire:frontend.partials.home-services-section />
     <livewire:frontend.partials.home-choose-section />
     <livewire:frontend.partials.home-work-process-section />
-    <livewire:frontend.partials.home-testimonial-section /> --}}
+    <livewire:frontend.partials.home-testimonial-section />
     <livewire:frontend.partials.home-blog-section />
 </main>
--- resources/views/livewire/frontend/partials/home-banner.blade.php
+++ resources/views/livewire/frontend/partials/home-banner.blade.php
@@ -29,10 +29,10 @@
                     <div class="banner-img-shape">
                         <img src="{{ Vite::asset('resources/frontend/images/shape2.png') }}" alt="Images">
                         <img src="{{ Vite::asset('resources/frontend/images/crm.png') }}" alt="Images">
-                        <img src="{{ Vite::asset('resources/frontend/images/facebook.png') }}" alt="Images">
+                        <img src="{{ Vite::asset('resources/frontend/images/voice5.png') }}" alt="Images">
                         <img src="{{ Vite::asset('resources/frontend/images/mail.png') }}" alt="Images">
                         <img src="{{ Vite::asset('resources/frontend/images/whatsapp.png') }}" alt="Images">
-                        <img src="{{ Vite::asset('resources/frontend/images/voice3.png') }}" alt="Images">
+                        <img src="{{ Vite::asset('resources/frontend/images/facebook.png') }}" alt="Images">
                     </div>
                 </div>
             </div>
