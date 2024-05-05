65c935eba19fa67480be03c326e3bad8305fb607
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-05-05 10:18:49 +0600

    set Slider model attributes

---
+++ database/migrations/2024_05_05_041004_create_sliders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title', 255);
+            $table->text('text');
+            $table->text('link');
+            $table->text('link_text');
+            $table->text('image');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sliders');
+    }
+};
