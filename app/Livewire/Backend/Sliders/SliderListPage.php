498347374e99e5e5f963ff44918ebcabe99870f0
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-05-14 11:57:28 +0600

    created livewire.backend.sliders.slider-list-page livewire component

---
+++ app/Livewire/Backend/Sliders/SliderListPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Backend\Sliders;
+
+use Livewire\Component;
+
+class SliderListPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.backend.sliders.slider-list-page');
+    }
+}
