108b0dace70bea35781b8b1c30871d3443b1d74e
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-05-21 22:57:34 +0600

    code optimized on AppserviceProvide and backend-component blade removed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Vite;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
