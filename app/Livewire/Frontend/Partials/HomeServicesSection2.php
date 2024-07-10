198eecc8929088fb73a40c68af6f0f4d010906a2
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-10 19:03:37 +0600

    added new home service section

---
+++ app/Livewire/Frontend/Partials/HomeServicesSection2.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Frontend\Partials;
+
+use Livewire\Component;
+
+/**
+ * @author Sakil Jomadder <sakil.diu.cse@gmail.com>
+ */
+class HomeServicesSection2 extends Component
+{
+    public function render()
+    {
+        return view('livewire.frontend.partials.home-services-section2');
+    }
+}
