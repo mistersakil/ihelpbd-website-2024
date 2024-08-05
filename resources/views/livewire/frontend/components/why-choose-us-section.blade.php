c9748acc4ea2958dceda4eb611a089ed048db2ba
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-05 14:47:42 +0600

    why choose us section done

--- resources/views/livewire/frontend/components/why-choose-us-section.blade.php
+++ resources/views/livewire/frontend/components/why-choose-us-section.blade.php
@@ -4,12 +4,14 @@
             <span class="sp-title">{{ $sectionTitle }}</span>
             <h2>{{ $sectionSubTitle }}</h2>
         </div>
+        <!-- /.section-title  -->
         <div class="row pt-45 align-items-center justify-content-center">
             <div class="col-lg-5">
                 <div class="choose-img">
                     <img src="{{ Vite::imageWeb('choose-img1.png') }}" alt="Choose Images">
                 </div>
             </div>
+            <!-- /.col -->
             <div class="col-lg-7">
                 <div class="choose-leftside">
                     <div class="row">
@@ -17,40 +19,50 @@
                             <div class="choose-card">
                                 <h3>Top Customer Service</h3>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-                                <i class="flaticon-customer-service"></i>
+                                <i class="{{ _icons('headset') }}"></i>
                                 <div class="circle"></div>
                             </div>
                         </div>
+                        <!-- /.col -->
                         <div class="col-lg-6 col-sm-6">
                             <div class="choose-card">
                                 <h3>Highly Personalized</h3>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-                                <i class="flaticon-document"></i>
+                                <i class="{{ _icons('document') }}"></i>
                                 <div class="circle"></div>
                             </div>
                         </div>
+                        <!-- /.col -->
                         <div class="col-lg-6 col-sm-6">
                             <div class="choose-card">
                                 <h3>Passionate Talent</h3>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-                                <i class="flaticon-user"></i>
+                                <i class="{{ _icons('user_heart') }}"></i>
                                 <div class="circle"></div>
                             </div>
                         </div>
+                        <!-- /.col -->
                         <div class="col-lg-6 col-sm-6">
                             <div class="choose-card">
                                 <h3>Quick Response Team</h3>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-                                <i class="flaticon-wall-clock"></i>
+                                <i class="{{ _icons('watch') }}"></i>
                                 <div class="circle"></div>
                             </div>
                         </div>
+                        <!-- /.col -->
                     </div>
+                    <!-- /.row -->
                     <div class="choose-bg">
                         <img src="{{ Vite::imageWeb('choose-bg.png') }}" alt="Choose Images">
                     </div>
+                    <!-- /.choose-bg -->
                 </div>
+                <!-- /.choose-leftside -->
             </div>
+            <!-- /.col -->
         </div>
+        <!-- /.row -->
     </div>
+    <!-- /.container -->
 </section>
