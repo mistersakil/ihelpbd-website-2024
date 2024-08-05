d3e58a40ff952a84bb4e5ca3432874e8cf0a1d7d
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-05 13:29:04 +0600

    working on why choose us section

---
+++ app/Livewire/Frontend/Components/WhyChooseUsSection.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Livewire\Frontend\Components;
+
+use Livewire\Component;
+use Illuminate\Contracts\View\View;
+
+/**
+ * @author Sakil Jomadder <sakil.diu.cse@gmail.com>
+ */
+class WhyChooseUsSection extends Component
+{
+    ## Component props
+    public array $dataList;
+    public string $sectionTitle;
+    public string $sectionSubTitle;
+    public string $isShowSectionHeader;
+    public int $limit;
+
+    ## Services
+    // private ProductService $productService;
+
+    public function boot()
+    {
+        // $this->productService = new ProductService;
+    }
+
+    /**
+     * Create a new component instance.
+     * @param string $sectionTitle `Title of the section`
+     * @param string $sectionSubTitle `Sub title of the section`
+     * @param int $limit `Number of items to display in the section`
+     * @return void
+     */
+    public function mount(string $sectionTitle = '', string $sectionSubTitle = '', int $limit = 6): void
+    {
+        $this->sectionTitle = $sectionTitle ? __($sectionTitle) : "";
+        $this->sectionSubTitle = $sectionSubTitle ? __($sectionSubTitle) : "";
+
+        $this->isShowSectionHeader = (!empty($this->sectionTitle) || !empty($this->sectionSubTitle)) ? true : false;
+
+        $this->limit = $limit;
+    }
+
+    /**
+     * Render view
+     *
+     * @return  \Illuminate\Contracts\View\View
+     */
+    public function render(): View
+    {
+        // $this->dataList = $this->productService->getStaticModels(limit: $this->limit);
+        return view('livewire.frontend.components.why-choose-us-section');
+    }
+}
---
+++ resources/views/livewire/frontend/components/why-choose-us-section.blade.php
@@ -0,0 +1,56 @@
+<section class="choose-area pt-100 pb-70">
+    <div class="container-fluid">
+        <div class="section-title text-center">
+            <span class="sp-title">{{ $sectionTitle }}</span>
+            <h2>{{ $sectionSubTitle }}</h2>
+        </div>
+        <div class="row pt-45 align-items-center justify-content-center">
+            <div class="col-lg-5">
+                <div class="choose-img">
+                    <img src="{{ Vite::imageWeb('choose-img1.png') }}" alt="Choose Images">
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="choose-leftside">
+                    <div class="row">
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="choose-card">
+                                <h3>Top Customer Service</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+                                <i class="flaticon-customer-service"></i>
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="choose-card">
+                                <h3>Highly Personalized</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+                                <i class="flaticon-document"></i>
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="choose-card">
+                                <h3>Passionate Talent</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+                                <i class="flaticon-user"></i>
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="choose-card">
+                                <h3>Quick Response Team</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+                                <i class="flaticon-wall-clock"></i>
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="choose-bg">
+                        <img src="{{ Vite::imageWeb('choose-bg.png') }}" alt="Choose Images">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
