e63a732673deca03139f83c10bdfe84f3cd769dc
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-07 00:03:26 +0600

    about us page about section done

---
+++ app/Livewire/Frontend/Components/AboutPageAboutSection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Livewire\Frontend\Components;
+
+use Livewire\Component;
+use Illuminate\Contracts\View\View;
+
+/**
+ * @author Sakil Jomadder <sakil.diu.cse@gmail.com>
+ */
+class AboutPageAboutSection extends Component
+{
+    /**
+     * Render view
+     *
+     * @return  \Illuminate\Contracts\View\View
+     */
+    public function render(): View
+    {
+        return view('livewire.frontend.components.about-page-about-section');
+    }
+}
---
+++ resources/views/livewire/frontend/components/about-page-about-section.blade.php
@@ -0,0 +1,42 @@
+<section class="about-area about-mt">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-lg-6">
+                <div class="about-img">
+                    <img src="{{ Vite::imageWeb('about-img1.jpg') }}" alt="Choose Images" />
+                    <div class="line">
+                        <img src="{{ Vite::imageWeb('about-line.png') }}" alt="Choose Images" />
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-lg-6">
+                <div class="about-content pl-20">
+                    <div class="section-title">
+                        <span class="sp-title">{{ __('about us') }}</span>
+                        <h2>Our Business is Your life Making Easier &amp; Comfortable</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ulabore et dolore magna aliqua. Quis ipsum suspendisse ultrices
+                            gravida. Risus commodo maecenas accumsan lacus vel facilisis.
+                        </p>
+                    </div>
+                    <!-- /.section-title -->
+                    <ul class="about-list">
+                        <li><i class="{{ _icons('arrow_right') }}"></i> Innovative Working Activities</li>
+                        <li><i class="{{ _icons('arrow_right') }}"></i> 100% Guarantee Issued for Client</li>
+                        <li><i class="{{ _icons('arrow_right') }}"></i> Dedicated Team Member</li>
+                        <li><i class="{{ _icons('arrow_right') }}"></i> Safe &amp; Secure Environment</li>
+                        <li><i class="{{ _icons('arrow_right') }}"></i> Responsive design</li>
+                        <li><i class="{{ _icons('arrow_right') }}"></i> Software security</li>
+                    </ul>
+                    <!-- /.about-list -->
+                </div>
+                <!-- /.about-content  -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</section>
