f35a2e5e7bbb01ec2a643bfe1abeb701aa85ffe5
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-06 23:19:35 +0600

    contact page contact card section added

---
+++ resources/views/livewire/frontend/components/contact-card-section.blade.php
@@ -0,0 +1,27 @@
+<section class="contact-card-area pt-100 pb-70">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-sm-6">
+                <div class="contact-card">
+                    <i class="{{ _icons('phone') }}"></i>
+                    <h3>Phone Number</h3>
+                    <p><a href="tel:+8256-569-887">+8801720092787</a></p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="contact-card">
+                    <i class="{{ _icons('email') }}"></i>
+                    <h3>Email Address</h3>
+                    <p><a href="mailto:hello@vconn.com">info@ihelpkl.com</a></p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="contact-card">
+                    <i class="{{ _icons('location') }}"></i>
+                    <h3>Location</h3>
+                    <p>950 St Johns Pl Brooklyn, NY, United States</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/livewire/frontend/contact/contact-page.blade.php
+++ resources/views/livewire/frontend/contact/contact-page.blade.php
@@ -2,5 +2,6 @@
     <x-slot:innerBanner>
         <x-frontend.layout.inner-banner :metaTitle="$metaTitle" :module="$module" />
     </x-slot:innerBanner>
-    <livewire:frontend.partials.home-about-section />
+
+    <livewire:frontend.components.contact-card-section />
 </main>
