e3987dabc5f0112dd1def382b0973f5c95939609
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-20 19:20:39 +0600

    redesign  solution details page

--- app/Services/AppService.php
+++ app/Services/AppService.php
@@ -14,58 +14,6 @@ use Illuminate\Support\Facades\Artisan;
  */
 class AppService
 {
-    /**
-     * generateUniqueKey method returns a table base custom unique key
-     *
-     * @return string
-     */
-    public function generateUniqueKey(string $tableName = '', int $keyLength = 8): string
-    {
-        $tmpKey = str_repeat("0", ($keyLength - 1)) . '1';
-        if (empty($tableName)) {
-            return $tmpKey;
-        }
-
-        $last_model = DB::table($tableName)->orderBy('id', 'desc')->first();
-
-        if ($last_model) {
-            $next_model_id = $last_model->id + 1;
-
-            for ($i = 1; $i < $keyLength; $i++) {
-                if (strlen($next_model_id) == $i) {
-                    $remaining_length = $keyLength - $i;
-                    $tmpKey = str_repeat("0", $remaining_length) . $next_model_id;
-                    break;
-                }
-            }
-            return $tmpKey;
-        }
-        return $tmpKey;
-    }
-
-    /**
-     * Get all records ID only of a given model.
-     *
-     * @param string $modelName
-     * @return \Illuminate\Support\Collection
-     * @throws \Exception
-     */
-    public function getModelIds($modelName): Collection
-    {
-        ## Make sure the model exists and can be resolved
-        if (!class_exists($modelName)) {
-            throw new Exception("Model not found: " . $modelName);
-        }
-
-        $model = new $modelName;
-
-        ## Ensure that the passed model name is an instance of Eloquent Model
-        if (!$model instanceof Model) {
-            throw new Exception("Invalid model: " . $modelName);
-        }
-
-        return $model::get()->pluck('id') ?? [];
-    }
 
     /**
      * Clear application cache
--- resources/views/livewire/frontend/solutions/solution-details-page.blade.php
+++ resources/views/livewire/frontend/solutions/solution-details-page.blade.php
@@ -2,66 +2,11 @@
     <x-slot:innerBanner>
         <x-frontend.layout.inner-banner :metaTitle="$metaTitle" :module="$module" />
     </x-slot:innerBanner>
-    <section class="service-details-area pt-100 pb-70">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="side-bar-area pr-20">
-                        <div class="side-bar-categories">
-                            <ul>
-                                @if (count($solutionList))
-                                    @foreach ($solutionList as $itemIndex => $item)
-                                        <li>
-                                            <a wire:navigate href="{{ $item['slug'] }}">
-                                                {{ ucwords($item['title']) }}
-                                                <i class="flaticon-arrow-pointing-to-right"></i>
-                                            </a>
-                                        </li>
-                                    @endforeach
-                                @endif
-                            </ul>
-                        </div>
-                        <!-- /.side-bar-categories  -->
-                    </div>
-                    <!-- /.side-bar-area  -->
-                </div>
-                <!-- /.col -->
-                <div class="col-lg-8">
-                    <div class="service-article">
-                        <div class="service-article-content">
-                            <img src="{{ $itemDetails['img_featured'] }}" alt="{{ ucwords($itemDetails['title']) }}">
-                            <div class="content">
-                                <h3>{{ ucwords($itemDetails['title']) }}</h3>
-                                <p>
-                                    {{ $itemDetails['body'] }}
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
-                                    gravida.
-                                    Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem Ipsum is simply
-                                    dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                                    industry's
-                                    standard dummy text ever since the 1500s, when an unknown printer took a galley of
-                                    type and scrambled it to make a type specimen book. It has survived not only five
-                                    centuries,
-                                    but also the leap into electronic typesetting, remaining essentially unchanged. It
-                                    was popularised in the 1960s with the release of Letraset sheets containing Lorem
-                                    Ipsum passages,
-                                    and more recently with desktop publishing software like Aldus PageMaker including
-                                    versions of Lorem Ipsum.
-                                </p>
-                            </div>
-                        </div>
-                        <!-- /.service-article-content -->
-                    </div>
-                    <!-- /.service-article -->
-                </div>
-                <!-- /.col -->
-            </div>
-            <!-- /.row -->
-        </div>
-        <!-- /.container -->
-    </section>
-    <!-- /.service-details-area -->
+    <livewire:frontend.partials.home-about-section />
+
+    <livewire:frontend.partials.home-projects />
+
+    <livewire:frontend.components.why-choose-us-section sectionTitle="why choose us"
+        sectionSubTitle="why you will give us priority" />
+
 </main>
