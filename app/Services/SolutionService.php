2067bfedbaf98b86e014838275b8b9eb8c084c07
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-27 16:06:47 +0600

    solution list page all section displayed

--- app/Services/SolutionService.php
+++ app/Services/SolutionService.php
@@ -373,7 +373,6 @@ class SolutionService
                         ]
                     ]
 
-
                 ]
             ],
 
--- resources/views/livewire/frontend/solutions/solution-list-page.blade.php
+++ resources/views/livewire/frontend/solutions/solution-list-page.blade.php
@@ -6,10 +6,10 @@
     <livewire:frontend.partials.solutions-section sectionTitle="solutions"
         sectionSubTitle="our provided best solutions" />
 
-    {{-- <livewire:frontend.partials.products-section sectionTitle="products" sectionSubTitle="our most valuable products"
-        :limit="3" /> --}}
+    <livewire:frontend.partials.products-section sectionTitle="products" sectionSubTitle="our most valuable products"
+        :limit="3" />
 
-    {{-- <livewire:frontend.components.why-choose-us-section sectionTitle="why choose us"
-        sectionSubTitle="why you will give us priority" /> --}}
+    <livewire:frontend.components.why-choose-us-section sectionTitle="why choose us"
+        sectionSubTitle="why you will give us priority" />
 
 </main>
