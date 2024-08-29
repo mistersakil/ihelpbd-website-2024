7c598dd3b44dddd200e39c3da0f01007ef69a943
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-29 13:13:12 +0600

    home page about section dynamic data displayed

--- app/Livewire/Frontend/Home/HomePage.php
+++ app/Livewire/Frontend/Home/HomePage.php
@@ -5,6 +5,7 @@ namespace App\Livewire\Frontend\Home;
 use Livewire\Component;
 use Livewire\Attributes\Title;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Vite;
 
 /**
  * @author Sakil Jomadder <sakil.diu.cse@gmail.com>
@@ -13,6 +14,34 @@ class HomePage extends Component
 {
     ## Meta props
     public string $metaTitle = 'home';
+    public array $data = [];
+
+
+    /**
+     * Create a new component instance.
+     * @return void
+     */
+    public function mount(): void
+    {
+        $this->data = [
+            'about' => [
+                'title' => 'About Project Management',
+                'subTitle' => 'Project Management section subtitle',
+                'img' => Vite::imageWeb('service-img1.jpg'),
+                'items' => [
+                    'point number one. point number one. point number one. ',
+                    'point number two. point number two. point number two',
+                    'point number three. point number three. point number three',
+                    'point number four. point number four. point number four',
+                    'point number five. point number five. point number five',
+                    'point number six. point number six. point number six',
+                    'point number seven. point number seven. point number seven',
+                    'point number eight. point number eight. point number eight',
+                    'point number nine. point number nine. point number nine',
+                ]
+            ]
+        ];
+    }
 
     /**
      * Render view
